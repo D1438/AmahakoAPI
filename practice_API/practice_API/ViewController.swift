efbc4dcc5befc2a049b9e878073d1c132241eaae
ishizawadaisuke <b1018172@fun.ac.jp>
2019-06-24 17:48:16 +0900

    2019/06/24 progress

---
+++ practice_API/practice_API/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practice_API
+//
+//  Created by Ishizawa Daisuke on 2019/06/24.
+//  Copyright © 2019 Ishizawa Daisuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
